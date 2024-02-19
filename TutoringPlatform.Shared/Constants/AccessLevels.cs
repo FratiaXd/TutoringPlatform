a762d3faa7ab64570e6001eb9fb02bfd3b3933c2
FratiaXd <welcomcherry@gmail.com>
2024-02-19 18:21:57 +0000

    sidebar displays course name dynamically

---
+++ TutoringPlatform.Shared/Constants/AccessLevels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoringPlatform.Shared.Constants
+{
+    public enum AccessLevel
+    {
+        Free,
+        Paid,
+        Restricted
+    }
+}
---
+++ TutoringPlatform/TutoringPlatform/Services/BuilderStateService.cs
@@ -0,0 +1,23 @@
+﻿namespace TutoringPlatform.Services
+{
+    public class BuilderStateService
+    {
+        private string _courseTitle = "Course draft";
+        public string CourseTitle
+        {
+            get => _courseTitle;
+            set
+            {
+                if (_courseTitle != value)
+                {
+                    _courseTitle = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+
+        public event Action OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
