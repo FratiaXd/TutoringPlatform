d73c32fc0b2dc7d0143f3e9dc847b33410e9e114
FratiaXd <welcomcherry@gmail.com>
2024-02-20 19:43:10 +0000

    moved input models to a separate files

---
+++ TutoringPlatform.Shared/InputModels/DetailsInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoringPlatform.Shared.InputModels
+{
+    public class DetailsInput
+    {
+        [Required, MaxLength(20), RegularExpression(@"^[a-zA-Z]+$")]
+        public string? FirstName { get; set; }
+        [Required, MaxLength(20), RegularExpression(@"^[a-zA-Z]+$")]
+        public string? LastName { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ TutoringPlatform.Shared/InputModels/PasswordInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutoringPlatform.Shared.InputModels
+{
+    public class PasswordInput
+    {
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; } = "";
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Admin password")]
+        public string AdminPassword { get; set; } = "";
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Password must contain at least one special character, one lowercase, one uppercase, and one digit. MinimumLength = 6", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; } = "";
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
