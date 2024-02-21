a15d9cc584f046706558f1cf7fa435fa41223e4a
FratiaXd <welcomcherry@gmail.com>
2024-02-21 17:37:14 +0000

    Fixed builderservice management and dynamic delete

--- TutoringPlatform/TutoringPlatform/Services/BuilderStateService.cs
+++ TutoringPlatform/TutoringPlatform/Services/BuilderStateService.cs
@@ -34,6 +34,13 @@ namespace TutoringPlatform.Services
             }
         }
 
+        public void ClearCourseState()
+        {
+            _courseId.Equals(0);
+            _lessons.Clear();
+            NotifyStateChanged();
+        }
+
         public void UpdateLesson(int lessonId, Lesson lesson)
         {
             _lessons[lessonId] = lesson;
@@ -47,6 +54,51 @@ namespace TutoringPlatform.Services
                 return _lessons[lessonId];
             }
             return null;
+        }
+
+        public void DeleteLesson(int lessonId)
+        {
+            if (_lessons.ContainsKey(lessonId))
+            {
+                var deletedOrder = _lessons[lessonId].LessonOrder;
+
+                _lessons.Remove(lessonId);
+
+                foreach (var lesson in _lessons.Values)
+                {
+                    if (lesson.LessonOrder > deletedOrder)
+                    {
+                        lesson.LessonOrder--;
+                    }
+                }
+                NotifyStateChanged();
+            }
+        }
+
+        public void DeleteQuiz(int quizId)
+        {
+            foreach (var lesson in _lessons.Values)
+            {
+                if (lesson.Quiz != null && lesson.Quiz.QuizId == quizId)
+                {
+                    lesson.Quiz = null; // Remove the quiz from the lesson
+                    NotifyStateChanged();
+                    return; // No need to continue searching if the quiz is found and removed
+                }
+            }
+        }
+
+        public void DeleteAssignment(int assignmentId)
+        {
+            foreach (var lesson in _lessons.Values)
+            {
+                if (lesson.Assignment != null && lesson.Assignment.AssignmentId == assignmentId)
+                {
+                    lesson.Assignment = null; // Remove the assignment from the lesson
+                    NotifyStateChanged();
+                    return; // No need to continue searching if the assignment is found and removed
+                }
+            }
         }
 
         public event Action OnChange;
