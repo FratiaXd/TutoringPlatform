2f499b39299844199f4929c9ff3b186a703bd405
FratiaXd <welcomcherry@gmail.com>
2024-03-19 00:01:33 +0000

    designed login page, fixed tailwind conflicts

--- TutoringPlatform/TutoringPlatform/tailwind.config.js
+++ TutoringPlatform/TutoringPlatform/tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
         "./node_modules/flowbite/**/*.js"
     ],
   theme: {
-    extend: {},
+      extend: {
+          spacing: {
+              '1': '0.25rem',
+              '2': '0.5rem',
+              '3': '0.75rem',
+              '5': '1.25rem',
+              '6': '1.5rem',
+              '7': '1.75rem',
+              '8': '2rem',
+              '9': '2.25rem',
+              '10': '2.5rem',
+              '11': '2.75rem',
+              '96': '24rem',
+              '112': '28rem',
+              '128': '32rem',
+          },
+      },
   },
     plugins: [
         require('flowbite/plugin')
